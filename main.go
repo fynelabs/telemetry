99e789c2b773732f2a49be98623ad2735f5f940e
Andy Williams <andy@andy.xyz>
2024-11-14 13:06:42 +0100

    Refactor our telemetry client and end sessions with the new Close function

--- main.go
+++ main.go
@@ -2,6 +2,8 @@ package telemetry
 
 import (
 	"fmt"
+	"io"
+	"log"
 	"math/rand"
 	"net/http"
 	"net/url"
@@ -23,6 +25,9 @@ type Telemetry struct {
 
 const prefUserKey = "fynelabs.telemetry.user"
 
+// Init opens a new telemetry instance and logs the start of a new session.
+// It uses the Fyne app to get the App ID and handle user uniqueness.
+// The `accessCode“ is the developer code for accessing Fyne Labs telemetry service.
 func Init(a fyne.App, accessCode string) *Telemetry {
 	id := a.UniqueID()
 
@@ -37,17 +42,27 @@ func Init(a fyne.App, accessCode string) *Telemetry {
 	return InitWithID(id, session.String(), user, accessCode)
 }
 
-func InitWithID(appID, session, user, accessCode string) *Telemetry {
+// InitWithID opens a new telemetry instance and logs the start of a new session.
+// The user of this must pass in a unique ID for the app, session and user.
+// The app ID refers to any instance of the app, the user ID should be consistent across launches
+// and the session should be unique for every invocation.
+// The `accessCode“ is the developer code for accessing Fyne Labs telemetry service.
+func InitWithID(appID, user, session, accessCode string) *Telemetry {
 	t := &Telemetry{AccessCode: accessCode, AppID: appID,
-		session: session, user: user, server: "https://xavier.fynelabs.com"}
+		user: user, server: "https://xavier.fynelabs.com"}
 
 	if env := os.Getenv("TELEMETRY_SERVER"); env != "" {
 		t.server = env
 	}
-	http.Get(fmt.Sprintf(t.server+"/api/v1/session?uuid=%s&appID=%s&user=%s",
-		session, appID, user))
 
+	t.sessionStart(session)
 	return t
+}
+
+// Close is used to shut down the telemetry instance, it will log that the session is ended.
+// This should be called at the end of an app's `main()` function.
+func (t *Telemetry) Close() {
+	t.sessionEnd()
 }
 
 func (t *Telemetry) Error(err error) {
@@ -64,16 +79,45 @@ func (t *Telemetry) Error(err error) {
 	}
 
 	encoded := url.QueryEscape(stack)
-	http.Get(fmt.Sprintf(t.server+"/api/v1/error?detail=%s&stack=%s&session=%s",
-		log, encoded, t.session))
+	t.send("error?detail=%s&stack=%s&session=%s", log, encoded, t.session)
 }
 
 func (t *Telemetry) Event(id string) {
-	http.Get(fmt.Sprintf(t.server+"/api/v1/event?name=%s&session=%s",
-		id, t.session))
+	t.send("event?name=%s&session=%s", id, t.session)
 }
 
 func (t *Telemetry) UserInfo(username, email string) {
-	http.Get(fmt.Sprintf(t.server+"/api/v1/user?uuid=%s&username=%s&email=%s",
-		t.user, username, email))
+	t.send("user?uuid=%s&username=%s&email=%s", t.user, username, email)
+}
+
+func (t *Telemetry) send(path string, params ...any) {
+	go func() {
+		url := fmt.Sprintf(t.server+"/api/v1/"+path, params...)
+		r, err := http.Get(url)
+
+		if err != nil {
+			log.Println("Failed to send telemetry", err)
+			return
+		}
+
+		data, err := io.ReadAll(r.Body)
+		if err != nil {
+			log.Println("Body read error", err)
+			return
+		}
+		r.Body.Close()
+		if len(data) > 0 {
+			log.Println("Body returned:", string(data))
+		}
+	}()
+}
+
+func (t *Telemetry) sessionEnd() {
+	t.send("sessionend?uuid=%s", t.session)
+}
+
+func (t *Telemetry) sessionStart(id string) {
+	t.session = id
+
+	t.send("session?uuid=%s&appID=%s&user=%s", id, t.AppID, t.user)
 }
